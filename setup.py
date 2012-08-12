6833ef4d15c8e7a9931a0fdd3a1fa0757e6862ee
Chris Lawlor <lawlor.chris@gmail.com>
2012-08-12 16:48:19 -0400

    Python packagification

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='django-athletics',
+      version='0.2',
+      description='Sports Team Management application for Django',
+      author='Chris Lawlor',
+      author_email='lawlor.chris@gmail.com',
+      url='https://github.com/chrislawlor/django-athletics',
+      download_url='https://github.com/chrislawlor/django-athletics/zipball/master',
+      packages=['athletics'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python'],
+      )
